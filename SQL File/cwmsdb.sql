c7ccb30e27d918f57f728f3ca41546f939e6c209
OMAR MOHAMMED NOOR <Hajirsugaal@gmail.com>
2022-11-26 04:35:36 -0800

    Update cwmsdb.sql

--- SQL File/cwmsdb.sql
+++ SQL File/cwmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 07:01 PM
+-- Generation Time: March 15, 2022 at 07:15 PM
 -- Server version: 10.3.15-MariaDB
 -- PHP Version: 7.2.19
 
